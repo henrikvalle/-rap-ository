f7bed26c2e35632aa2c4ad2be0c4b78bb8fb25ec
Henrique Valle <henriquevtmelo@gmail.com>
2019-10-22 16:09:37 -0300

    forca.h

---
+++ FORCA/forca.h
@@ -0,0 +1,16 @@
+#ifndef FORCA_H_INCLUDED
+#define FORCA_H_INCLUDED
+
+typedef struct noSecreto NoSecreto;
+typedef struct noLetras  NoL;
+NoSecreto *  inicializaListaSecreta();
+NoSecreto * carregaListaArquivo(NoSecreto * l, char nomeArq[255]);
+void imprimeListaSecreta(NoSecreto *l);
+NoSecreto * inserePalavraSecreta(NoSecreto * l,char word[31],char subject [100]);
+int temPalavraNaoUsada(NoSecreto *l);
+NoSecreto * sorteiaPalavra(NoSecreto *l);
+int quantos(NoSecreto * l);
+NoL * crialetras(NoSecreto * l);
+NoSecreto * retornaPalavraPos(NoSecreto *l, int pos);
+int geraAleatorio(int maximo);
+#endif // FORCA_H_INCLUDED
---
+++ FORCA/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "forca.h"
+#include "forca.c"
+void exibe()
+{
+    printf("Desenvolvido por Henrique Valle e Leticia Marinho\n\n");
+}
+
+int main()
+{
+    exibe();
+    NoSecreto * lstSecreta = inicializaListaSecreta();
+    NoSecreto * sorteada;
+    lstSecreta = carregaListaArquivo(lstSecreta,"palavras.dat");
+    imprimeListaSecreta(lstSecreta);
+
+      do{
+        system("cls");
+        exibe();
+        imprimeListaSecreta(lstSecreta);
+
+        sorteada= sorteiaPalavra(lstSecreta);
+        if(sorteada!=NULL){
+            printf("%s\n", sorteada->palavra);
+        }else{
+            printf("Não tem palavra disponível!\n\n");
+        }
+    }while(getchar()!='f');
+
+
+
+    NoL * letras = inicializaListaSecreta();
+
+    return 0;
+}
